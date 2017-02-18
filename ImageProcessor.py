ae2a3bca7e9876462c0c46204c4eaaa2ad272b75
jbi <biehler@adco-engineering-gw.com>
2017-02-18 14:49:11 +0100

    got first working pipeline for lane detection working using sliding windows technique added several drawing functions to plot detected lane onto original image added polynomial fitting of lane lines added option to save camera calibration data to pickle file

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -2,6 +2,9 @@ import numpy as np
 import cv2
 import matplotlib.image as mpimg
 import glob
+import matplotlib.pyplot as plt
+import pickle
+import os
 
 class ImageProcessor:
     def __init__(self):
@@ -21,7 +24,7 @@ class ImageProcessor:
         # Inverse Perspective Transform matrix
         self.inverse_transform_matrix = cv2.getPerspectiveTransform(self.dst_coords, self.src_coords)
 
-    def calibrate_camera(self):
+    def compute_camera_calibration(self):
         """
             Perform camera calibration using the calibration images from the camera_cal folder.
             These images are used to calculate the camera matrix and distortion coefficients.
@@ -52,6 +55,19 @@ class ImageProcessor:
         # detected chessboard corners
         cv2.calibrateCamera(object_points, image_points, img.shape[0:2], self.camera_matrix, self.camera_distortion_coefficients)
 
+    def calibrate_camera(self):
+        # if calibration data is already available
+        if os.path.exists("camera_calibration_data.pickle"):
+            with open("camera_calibration_data.pickle", "rb") as f:
+                self.camera_matrix = pickle.load(f)
+                self.camera_distortion_coefficients = pickle.load(f)
+        # otherwise: calculate calibration data and save to pickle.
+        else:
+            self.compute_camera_calibration()
+            with open("camera_calibration_data.pickle", "wb") as f:
+                pickle.dump(self.camera_matrix, f)
+                pickle.dump(self.camera_distortion_coefficients, f)
+
         # set calibration flag to true
         self.iscalibrated = True
 
@@ -68,7 +84,17 @@ class ImageProcessor:
         img_size = (img.shape[1], img.shape[0])
         return cv2.warpPerspective(img, self.transform_matrix, img_size, flags=cv2.INTER_LINEAR)
 
+    def reverse_perspective_transform(self,img):
+        """
+            reverse perspective transformation to image
+        """
+        img_size = (img.shape[1], img.shape[0])
+        return cv2.warpPerspective(img, self.inverse_transform_matrix, img_size, flags=cv2.INTER_LINEAR)
+
     def compute_binary_thresholded_image(self,img):
+        """
+            apply thresholding using various techniques to image and return binary image
+        """
         # parameters, make arguments later on
         # threshold x-gradient
         min_grad = 30
@@ -110,3 +136,141 @@ class ImageProcessor:
 
         #return color_binary, combined_binary
         return combined_binary
+
+    def compute_histogram_with_peaks(self,binary_warped):
+        """
+            compute a histogram of lower half of binary images and compute
+            two peaks of the histogram as starting point for lane detection
+        """
+        histogram = np.sum(binary_warped[binary_warped.shape[0]/2:,:], axis=0)
+        midpoint = np.int(histogram.shape[0]/2)
+        # search for peak left and right from center
+        leftx_base = np.argmax(histogram[:midpoint])
+        rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+        peaks = [leftx_base,rightx_base]
+        return peaks, histogram
+
+    def get_lane_lines_pixels_using_sliding_windows(self,binary_warped):
+        """
+            search for lane line pixels using sliding windows
+        """
+        # compute histogram to compute base points
+        peaks, histogram = self.compute_histogram_with_peaks(binary_warped)
+        leftx_base = peaks[0]
+        rightx_base = peaks[1]
+        # using code snipet from lecture for this
+        # Create an output image to draw on and  visualize the result
+        out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+
+        # Choose the number of sliding windows
+        nwindows = 9
+        # Set height of windows
+        window_height = np.int(binary_warped.shape[0]/nwindows)
+        # Identify the x and y positions of all nonzero pixels in the image
+        nonzero = binary_warped.nonzero()
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Current positions to be updated for each window
+        leftx_current = leftx_base
+        rightx_current = rightx_base
+        # Set the width of the windows +/- margin
+        margin = 100
+        # Set minimum number of pixels found to recenter window
+        minpix = 50
+        # Create empty lists to receive left and right lane pixel indices
+        left_lane_inds = []
+        right_lane_inds = []
+
+        # Step through the windows one by one
+        for window in range(nwindows):
+            # Identify window boundaries in x and y (and right and left)
+            win_y_low = binary_warped.shape[0] - (window+1)*window_height
+            win_y_high = binary_warped.shape[0] - window*window_height
+            win_xleft_low = leftx_current - margin
+            win_xleft_high = leftx_current + margin
+            win_xright_low = rightx_current - margin
+            win_xright_high = rightx_current + margin
+            # Draw the windows on the visualization image
+            #cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),(0,255,0), 2)
+            #cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),(0,255,0), 2)
+            # Identify the nonzero pixels in x and y within the window
+            good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+            good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+            # Append these indices to the lists
+            left_lane_inds.append(good_left_inds)
+            right_lane_inds.append(good_right_inds)
+            # If you found > minpix pixels, recenter next window on their mean position
+            if len(good_left_inds) > minpix:
+                leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+            if len(good_right_inds) > minpix:
+                rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+        # Concatenate the arrays of indices
+        left_lane_inds = np.concatenate(left_lane_inds)
+        right_lane_inds = np.concatenate(right_lane_inds)
+
+        # Extract left and right line pixel positions
+        leftx = nonzerox[left_lane_inds]
+        lefty = nonzeroy[left_lane_inds]
+        rightx = nonzerox[right_lane_inds]
+        righty = nonzeroy[right_lane_inds]
+
+        # return pixels identified as lane lines
+        return leftx, lefty, rightx, righty, left_lane_inds, right_lane_inds
+
+    def get_lane_line_pixels_using_previous_fit(self,binary_warped,left_fit,right_fit):
+        """
+            search for lane line pixels using result from previous fit
+        """
+        # Assume you now have a new warped binary image
+        # from the next frame of video (also called "binary_warped")
+        # It's now much easier to find line pixels!
+        nonzero = binary_warped.nonzero()
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        margin = 100
+        left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2] - margin)) & (nonzerox < (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2] + margin)))
+        right_lane_inds = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2] - margin)) & (nonzerox < (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2] + margin)))
+
+        # Again, extract left and right line pixel positions
+        leftx = nonzerox[left_lane_inds]
+        lefty = nonzeroy[left_lane_inds]
+        rightx = nonzerox[right_lane_inds]
+        righty = nonzeroy[right_lane_inds]
+
+        # return pixels identified as lane lines
+        return leftx, lefty, rightx, righty, left_lane_inds, right_lane_inds
+
+
+    def draw_lanes_on_road(self,undist, warped, ploty, left_fitx, right_fitx ):
+        """
+            draw detected lane area onto original image
+        """
+        # Create an image to draw the lines on
+        color_warp = np.zeros_like(warped).astype(np.uint8)
+
+        # Recast the x and y points into usable format for cv2.fillPoly()
+        pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+        pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+        pts = np.hstack((pts_left, pts_right))
+
+        # Draw the lane onto the warped blank image
+        cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+
+        # Warp the blank back to original image space using inverse perspective matrix (Minv)
+        newwarp=self.reverse_perspective_transform(color_warp)
+
+        # Combine the result with the original image
+        return cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
+
+    def add_curve_radius_and_car_pos_to_images(self,img,position,left_radius,right_radius):
+        """
+            annotate image with car position and lane line radii
+        """
+        # add text on top of images
+        left_offset = int(img.shape[1]/12)
+        height_offset = int(img.shape[0]/10)
+        cv2.putText(img, "Position: %.4f m" % position, (left_offset, height_offset + 0*40), cv2.FONT_HERSHEY_SIMPLEX, 1, [255, 255, 255], 2)
+        cv2.putText(img, "Left radius: %.2f m" % left_radius,(left_offset, height_offset + 1*40), cv2.FONT_HERSHEY_SIMPLEX, 1, [255, 255, 255], 2)
+        cv2.putText(img, "Right radius: %.2f m" % right_radius, (left_offset, height_offset + 2*40), cv2.FONT_HERSHEY_SIMPLEX, 1, [255, 255, 255], 2)
+        return img
--- LaneLines.py
+++ LaneLines.py
@@ -2,9 +2,11 @@ from ImageProcessor import ImageProcessor
 import cv2
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
+import numpy as np
 import glob
 import argparse
 from moviepy.editor import VideoFileClip
+
 # Define a class to handle lane line detection
 class LaneLines:
     def __init__(self):
@@ -20,17 +22,106 @@ class LaneLines:
             mpimg.imsave('transformed_images/'+file_name,color_binary)
             mpimg.imsave('transformed_images/'+file_name,combined_binary)
 
+    def apply_pipeline(self,img):
+        undistorded_img = self.my_image_processor.undistort_image(img)
+        processed_image = self.process_image(img)
+        #return cv2.cvtColor(processed_image*255, cv2.COLOR_GRAY2RGB)
+        result, left_fitx, right_fitx, ploty, left_curverad, right_curverad = self.fit_lane_lines(processed_image)
+
+        final_result = my_lanes_lines.my_image_processor.draw_lanes_on_road(undistorded_img,result,ploty,left_fitx,right_fitx)
+        final_result = my_lanes_lines.my_image_processor.add_curve_radius_and_car_pos_to_images(final_result,0.0,left_curverad,right_curverad)
+        return final_result
+
     def process_image(self, img):
-        undistorted_img = my_lanes_lines.my_image_processor.undistort_image(img)
-        thresholded_img = my_lanes_lines.my_image_processor.compute_binary_thresholded_image(undistorted_img)
-        transformed_img = my_lanes_lines.my_image_processor.apply_perspective_transform(thresholded_img)
-        # transform binary to grayscale
-        return cv2.cvtColor(transformed_img*255, cv2.COLOR_GRAY2RGB)
-        #return transformed_img
+        undistorted_img = self.my_image_processor.undistort_image(img)
+        thresholded_img = self.my_image_processor.compute_binary_thresholded_image(undistorted_img)
+        transformed_img = self.my_image_processor.apply_perspective_transform(thresholded_img)
+        return transformed_img
 
+    def fit_lane_lines(self,transformed_img):
+        leftx, lefty, rightx, righty, left_lane_inds, right_lane_inds = self.my_image_processor.get_lane_lines_pixels_using_sliding_windows(transformed_img)
+        # Fit a second order polynomial to each
+        left_fit = np.polyfit(lefty, leftx, 2)
+        right_fit = np.polyfit(righty, rightx, 2)
+        # Generate x and y values for plotting
+        ploty = np.linspace(0, transformed_img.shape[0]-1, transformed_img.shape[0] )
+        left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+        right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
 
+        # temporay plot results
+        # Create an image to draw on and an image to show the selection window
+        out_img = np.dstack((transformed_img, transformed_img, transformed_img))*255
+        window_img = np.zeros_like(out_img)
 
-# try on images when called as main:
+        nonzero = transformed_img.nonzero()
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Color in left and right line pixels
+        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+        out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+
+        # Generate a polygon to illustrate the search window area
+        # And recast the x and y points into usable format for cv2.fillPoly()
+        # Set the width of the windows +/- margin
+        # TODO change later
+        margin = 100
+        left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
+        left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx+margin, ploty])))])
+        left_line_pts = np.hstack((left_line_window1, left_line_window2))
+        right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
+        right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx+margin, ploty])))])
+        right_line_pts = np.hstack((right_line_window1, right_line_window2))
+
+        # Draw the lane onto the warped blank image
+        cv2.fillPoly(window_img, np.int_([left_line_pts]), (0,255, 0))
+        cv2.fillPoly(window_img, np.int_([right_line_pts]), (0,255, 0))
+        result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+        plt.imshow(result)
+        plt.plot(left_fitx, ploty, color='yellow')
+        plt.plot(right_fitx, ploty, color='yellow')
+        plt.xlim(0, 1280)
+        plt.ylim(720, 0)
+        left_curverad, right_curverad = self.compute_curvature(ploty,left_fitx,right_fitx)
+        return result, left_fitx, right_fitx, ploty, left_curverad, right_curverad
+
+    def compute_curvature(self,ploty,leftx,rightx):
+        y_eval = np.max(ploty)
+        # Define conversions in x and y from pixels space to meters
+        ym_per_pix = 30/720 # meters per pixel in y dimension
+        xm_per_pix = 3.7/700 # meters per pixel in x dimension
+
+        # Fit new polynomials to x,y in world space
+        left_fit_cr = np.polyfit(ploty*ym_per_pix, leftx*xm_per_pix, 2)
+        right_fit_cr = np.polyfit(ploty*ym_per_pix, rightx*xm_per_pix, 2)
+        # Calculate the new radii of curvature
+        left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+        right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+        return left_curverad, right_curverad
+
+    def process_test_images(self):
+        #for i,img_name in enumerate(("camera_cal/calibration3.jpg", "test_images/straight_lines1.jpg")):
+        for img_name in glob.glob("test_images/*.jpg"):
+            print(img_name)
+            img = mpimg.imread(img_name)
+            undistorded_img = my_lanes_lines.my_image_processor.undistort_image(img)
+            processed_image = my_lanes_lines.process_image(img)
+            result, left_fitx, right_fitx, ploty, left_curverad, right_curverad = my_lanes_lines.fit_lane_lines(processed_image)
+            #plt.imshow(result)
+            #plt.show()
+            final_result = my_lanes_lines.my_image_processor.draw_lanes_on_road(undistorded_img,result,ploty,left_fitx,right_fitx)
+            final_result = my_lanes_lines.my_image_processor.add_curve_radius_and_car_pos_to_images(final_result,0.0,left_curverad,right_curverad)
+            plt.imshow(final_result)
+            plt.show()
+
+            output = cv2.cvtColor(processed_image*255, cv2.COLOR_GRAY2RGB)
+            mpimg.imsave('processed_images/'+img_name, output)
+
+    def process_test_video(self,input_file,output_file,start=0.0,end=2.0):
+        video = VideoFileClip(input_file)
+        #video = video.subclip(t_start=start, t_end=end)
+        processed_video = video.fl_image(self.apply_pipeline)
+        processed_video.write_videofile(output_file,audio=False)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Advanced lane line detection")
     parser.add_argument('--input', type=str, default='project_video.mp4', help='input video')
@@ -53,23 +144,5 @@ if __name__ == '__main__':
 
     my_lanes_lines=LaneLines()
 
-    video = VideoFileClip(input_file)
-    avideo = video.subclip(t_start=00.0, t_end=00.10)
-    processed_video = video.fl_image(my_lanes_lines.process_image)
-    processed_video.write_videofile(output_file, audio=False)
-
-    #for i,img_name in enumerate(("camera_cal/calibration3.jpg", "test_images/straight_lines1.jpg")):
-    for img_name in glob.glob("test_images/*.jpg"):
-        print(img_name)
-        img = mpimg.imread(img_name)
-        processed_image = my_lanes_lines.process_image(img)
-        mpimg.imsave('processed_images/'+img_name, processed_image)
-    #    # undistort images
-    #    udist_img = my_lanes_lines.my_image_processor.undistort_image(img)
-    #    mpimg.imsave('undistorted_images/'+img_name,udist_img)
-    #
-    #        thresholded_img = my_lanes_lines.my_image_processor.compute_binary_thresholded_image(udist_img)
-    #        # apply perspective transform
-    #        transformed_img = my_lanes_lines.my_image_processor.apply_perspective_transform(thresholded_img)
-    #        mpimg.imsave('transformed_images/'+img_name,transformed_img)
-    #        ## compute binary thresholded imanges
+    my_lanes_lines.process_test_video(input_file,output_file)
+    #my_lanes_lines.process_test_images()
