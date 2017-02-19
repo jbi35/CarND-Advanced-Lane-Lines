e1b7c8e292abae88ea18088810940e3ef01cc939
jbi <biehler@adco-engineering-gw.com>
2017-02-19 12:27:02 +0100

    further changes and cleanup

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -14,7 +14,6 @@ class ImageProcessor:
         self.camera_distortion_coefficients = np.zeros(5)
         # flag indicating whether camera has been calibrated
         self.iscalibrated = False
-
         # source coordinates for perspective transform
         self.src_coords = np.float32([[594,449],[689,449],[1114,719],[215,719]])
         # destination coordinates for perspective transform
@@ -23,6 +22,9 @@ class ImageProcessor:
         self.transform_matrix = cv2.getPerspectiveTransform(self.src_coords, self.dst_coords)
         # Inverse Perspective Transform matrix
         self.inverse_transform_matrix = cv2.getPerspectiveTransform(self.dst_coords, self.src_coords)
+
+        # call upon construction
+        self.calibrate_camera()
 
     def compute_camera_calibration(self):
         """
@@ -56,6 +58,9 @@ class ImageProcessor:
         cv2.calibrateCamera(object_points, image_points, img.shape[0:2], self.camera_matrix, self.camera_distortion_coefficients)
 
     def calibrate_camera(self):
+        """
+            Calibrate camera either load calibration data or perform calibration using images
+        """
         # if calibration data is already available
         if os.path.exists("camera_calibration_data.pickle"):
             with open("camera_calibration_data.pickle", "rb") as f:
@@ -133,8 +138,6 @@ class ImageProcessor:
         # Combine the two binary images
         combined_binary = np.zeros_like(scaled_grad_x_binary)
         combined_binary[(s_channel_binary == 1) | (scaled_grad_x_binary == 1)] = 1
-
-        #return color_binary, combined_binary
         return combined_binary
 
     def compute_histogram_with_peaks(self,binary_warped):
@@ -149,98 +152,6 @@ class ImageProcessor:
         rightx_base = np.argmax(histogram[midpoint:]) + midpoint
         peaks = [leftx_base,rightx_base]
         return peaks, histogram
-
-    def get_lane_lines_pixels_using_sliding_windows(self,binary_warped):
-        """
-            search for lane line pixels using sliding windows
-        """
-        # compute histogram to compute base points
-        peaks, histogram = self.compute_histogram_with_peaks(binary_warped)
-        leftx_base = peaks[0]
-        rightx_base = peaks[1]
-        # using code snipet from lecture for this
-        # Create an output image to draw on and  visualize the result
-        out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
-
-        # Choose the number of sliding windows
-        nwindows = 9
-        # Set height of windows
-        window_height = np.int(binary_warped.shape[0]/nwindows)
-        # Identify the x and y positions of all nonzero pixels in the image
-        nonzero = binary_warped.nonzero()
-        nonzeroy = np.array(nonzero[0])
-        nonzerox = np.array(nonzero[1])
-        # Current positions to be updated for each window
-        leftx_current = leftx_base
-        rightx_current = rightx_base
-        # Set the width of the windows +/- margin
-        margin = 100
-        # Set minimum number of pixels found to recenter window
-        minpix = 50
-        # Create empty lists to receive left and right lane pixel indices
-        left_lane_inds = []
-        right_lane_inds = []
-
-        # Step through the windows one by one
-        for window in range(nwindows):
-            # Identify window boundaries in x and y (and right and left)
-            win_y_low = binary_warped.shape[0] - (window+1)*window_height
-            win_y_high = binary_warped.shape[0] - window*window_height
-            win_xleft_low = leftx_current - margin
-            win_xleft_high = leftx_current + margin
-            win_xright_low = rightx_current - margin
-            win_xright_high = rightx_current + margin
-            # Draw the windows on the visualization image
-            #cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),(0,255,0), 2)
-            #cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),(0,255,0), 2)
-            # Identify the nonzero pixels in x and y within the window
-            good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
-            good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
-            # Append these indices to the lists
-            left_lane_inds.append(good_left_inds)
-            right_lane_inds.append(good_right_inds)
-            # If you found > minpix pixels, recenter next window on their mean position
-            if len(good_left_inds) > minpix:
-                leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
-            if len(good_right_inds) > minpix:
-                rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
-
-        # Concatenate the arrays of indices
-        left_lane_inds = np.concatenate(left_lane_inds)
-        right_lane_inds = np.concatenate(right_lane_inds)
-
-        # Extract left and right line pixel positions
-        leftx = nonzerox[left_lane_inds]
-        lefty = nonzeroy[left_lane_inds]
-        rightx = nonzerox[right_lane_inds]
-        righty = nonzeroy[right_lane_inds]
-
-        # return pixels identified as lane lines
-        return leftx, lefty, rightx, righty, left_lane_inds, right_lane_inds
-
-    def get_lane_line_pixels_using_previous_fit(self,binary_warped,left_fit,right_fit):
-        """
-            search for lane line pixels using result from previous fit
-        """
-        # Assume you now have a new warped binary image
-        # from the next frame of video (also called "binary_warped")
-        # It's now much easier to find line pixels!
-        nonzero = binary_warped.nonzero()
-        nonzeroy = np.array(nonzero[0])
-        nonzerox = np.array(nonzero[1])
-        margin = 100
-        left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2] - margin)) & (nonzerox < (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2] + margin)))
-        right_lane_inds = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2] - margin)) & (nonzerox < (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2] + margin)))
-
-        # Again, extract left and right line pixel positions
-        leftx = nonzerox[left_lane_inds]
-        lefty = nonzeroy[left_lane_inds]
-        rightx = nonzerox[right_lane_inds]
-        righty = nonzeroy[right_lane_inds]
-
-        # return pixels identified as lane lines
-        return leftx, lefty, rightx, righty, left_lane_inds, right_lane_inds
-
 
     def draw_lanes_on_road(self,undist, warped, ploty, left_fitx, right_fitx ):
         """
